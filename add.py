2a4989b1a2b1a6cfa2b164dfcf36d8c01bd462df
Muhammad Aakif K F <148759553+aakif7k@users.noreply.github.com>
2024-12-23 14:00:03 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,11 @@
+# Program to add two numbers
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Displaying the sum
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
